70721454ccb3b35d2899e014eb50dce19f425e5d
Amir Khalili <a.khalili64@gmail.com>
2023-09-15 12:00:45 +0330

    Project restructure. camera file added.

---
+++ src/camera.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+use bevy_third_person_camera::*;
+
+pub fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        ThirdPersonCamera {
+           zoom: Zoom::new(5.0, 10.0),
+        ..default()
+        },
+        Camera3dBundle::default()
+    ))
+    .insert(Camera)
+    .insert(Name::new("Camera"));
+}
